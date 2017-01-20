29030bf187f09db65aee39d79a1e05bdcd142e71
Tony Minott <minottanthonya@gmail.com>
2017-01-20 13:02:04 -0500

    Second commit

--- 0.01_firsteuler.js
+++ 0.01_firsteuler.js
@@ -1,10 +1,10 @@
-//problem 1 Add all the natural numbers below one thousand that are multiples of 3 or 5.
-var total = 0;
+//problem 1: Add all the natural numbers below one thousand that are multiples of 3 or 5.
+var answer = 0;
 
 for(var i = 0; i < 1000; i++) {
   if(i % 3 == 0 || i % 5 == 0) {
-    total += i;
+    answer += i;
   }
 }
 
-console.log(total);
+console.log(answer);
