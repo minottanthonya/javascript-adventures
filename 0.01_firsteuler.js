0a0eb890bfb21ef65daca39013b29604b4c6e13b
Tony Minott <minottanthonya@gmail.com>
2017-01-20 12:47:06 -0500

    Initial Commit

---
+++ 0.01_firsteuler.js
@@ -0,0 +1,10 @@
+//problem 1 Add all the natural numbers below one thousand that are multiples of 3 or 5.
+var total = 0;
+
+for(var i = 0; i < 1000; i++) {
+  if(i % 3 == 0 || i % 5 == 0) {
+    total += i;
+  }
+}
+
+console.log(total);
