72fc3133d9e3eff7b2f2ffe9e1a32dd3df1df759
Tony Minott <minottanthonya@gmail.com>
2017-01-29 16:46:37 -0500

    Worked on For Loops

--- 0.01_js_basics.js
+++ 0.01_js_basics.js
@@ -1,1 +1,21 @@
-console.log
+// Practice For Loop
+
+//1. Write a for loop that will iterate from 0 to 20. For each iteration, 
+// it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+
+for (var i = 0; i <= 20; i ++ ) {
+	if (i % 2 === 0) {
+		console.log(i + ' is even');
+	} else {
+		console.log(i + ' is odd');
+	}
+}
+
+// 2. Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, 
+// it will multiply the number by 9 and log the result. 
+
+var multiplier = 9;
+for (var i = 0; i <= 10; i++) {
+    var result = multiplier * i;
+    console.log(multiplier + ' * ' + i + ' = ' + result);
+}
