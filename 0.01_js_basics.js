6c2eb4f933c344341d94327910ff51cb25cb6c24
Tony Minott <minottanthonya@gmail.com>
2017-02-24 10:33:16 -0500

    White Boarding stuff

--- 0.01_js_basics.js
+++ 0.01_js_basics.js
@@ -1,1 +1,8 @@
-console.log
+// create a function with 2 parametets, first and last. have it pring to the console
+// "your first name is: first, you last name is: last" using a string literal remember string literals
+// accepts valies inside of a string use this symbol: ${}
+
+function name(first, last){
+  console.log(`Your first name is: ${first} Your last name is: ${last}`)
+}
+name("Tony", "Minott")
