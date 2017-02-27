1c46da474314f89ab610adea1299b5e7c1f6e515
Tony Minott <minottanthonya@gmail.com>
2017-02-27 15:43:42 -0500

    Finished another lesson: object literals

--- 0.04_jsbasics.js
+++ 0.04_jsbasics.js
@@ -1,2 +1,16 @@
 // Create an object literal called user. Inside this object create a username and password parameter. 
 // Also, create a function call isLoggedIn that console logs or returns logged in.
+
+var user = {
+  username : "",
+  password : "",
+
+  isLoggedIn : function(){
+    console.log(this.username + " is logged in.");
+  }
+}
+
+var ted = Object.create(user);
+ted.username = "TedisDead";
+
+ted.isLoggedIn();
