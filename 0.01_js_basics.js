f69ef292d7901c62a49a94ecaf403126e005d84b
Tony Minott <minottanthonya@gmail.com>
2017-02-27 16:04:53 -0500

    More for loop practice

--- 0.01_js_basics.js
+++ 0.01_js_basics.js
@@ -1,12 +1,3 @@
-// create a function with 2 parametets, first and last. have it pring to the console
-// "your first name is: first, you last name is: last" using a string literal remember string literals
-// accepts valies inside of a string use this symbol: ${}
-
-function name(first, last){
-  console.log(`Your first name is: ${first} Your last name is: ${last}`)
-}
-name("Tony", "Minott")
-=======
 // Practice For Loop
 
 //1. Write a for loop that will iterate from 0 to 20. For each iteration, 
--- 0.05_jsbasics.js
+++ 0.05_jsbasics.js
@@ -1,1 +1,6 @@
-// Create a for loop that that starts at zero and ends at 20, but counts by 2’s. 
+// Create a for loop that that starts at 
+// zero and ends at 20, but counts by 2’s. 
+
+for (var i = 0; i <= 20; i += 2 ) {
+  console.log(i);
+}
