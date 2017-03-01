ccad0ad0845fc7a0c0f17a8b61c74d16d09dc8a6
Tony Minott <minottanthonya@gmail.com>
2017-02-28 23:08:34 -0500

    I CAN CODE, kinda

--- 0.02_jsbasics.js
+++ 0.02_jsbasics.js
@@ -1,4 +1,4 @@
-// create a function with 2 parametets, first and last. have it pring to the console
+// create a function with 2 parameters, first and last. have it pring to the console
 // "your first name is: first, you last name is: last" using a string literal remember string literals
 // accepts valies inside of a string use this symbol: ${}
 
@@ -6,3 +6,12 @@ function name(first, last){
   console.log(`Your first name is: ${first} Your last name is: ${last}`)
 }
 name("Tony", "Minott")
+
+// Write a function that accepts two numbers as parameters and then returns or console.logs the product of them.
+
+function mult(num1, num2){
+	return num1*num2;
+}
+var product = mult(2,2);
+
+console.log(product);
